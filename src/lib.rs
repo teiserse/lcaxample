ccbad33d1c67fc3d9a7672e08b7bcabc9e3eee0c
teiserse <teiserse@tcd.ie>
2018-09-23 11:59:06 +0100

    Gave the initial definition of the tree and a test constructor.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,42 @@
+pub mod tree {
+    #[derive(Debug)]
+    pub struct BTree<T: PartialOrd> {
+        root: Option<Box<BNode<T>>>,
+    }
+
+    impl<T: PartialOrd> BTree<T> {
+        pub fn new(value: Option<T>) -> BTree<T> {
+            match value {
+                None => BTree { root: None },
+                Some(x) => BTree {
+                    root: Some(Box::new(BNode {
+                        parent: None,
+                        value: x,
+                        left_child: None,
+                        right_child: None,
+                    }))
+                },
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    struct BNode<T: PartialOrd> {
+        parent: Option<Box<BNode<T>>>,
+        value: T,
+        left_child: Option<Box<BNode<T>>>,
+        right_child: Option<Box<BNode<T>>>,
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use tree::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_construction() {
+        let a: BTree<i32> = BTree::new(None);
+        let b: BTree<i32> = BTree::new(Some(5));
+        println!("{:?}\n{:?}", a, b);
     }
 }
