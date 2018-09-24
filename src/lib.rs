2999bb80350c817745762d488d6b5026cccc820f
teiserse <teiserse@tcd.ie>
2018-09-24 11:48:04 +0100

    Partial completion of a non-ugly way to display the tree (if the element supports it)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+
 pub mod tree {
     #[derive(Debug)]
     pub struct BTree<T: PartialOrd> {
@@ -20,12 +21,25 @@ pub mod tree {
 
         pub fn insert(&mut self, value: T) {
             match self.root {
-                None => {self.root = Some(Box::new(BNode {
-                    value,
-                    left_child: None,
-                    right_child: None,
-                }))}
-                Some(ref mut x) => {x.insert(value);}
+                None => {
+                    self.root = Some(Box::new(BNode {
+                        value,
+                        left_child: None,
+                        right_child: None,
+                    }))
+                }
+                Some(ref mut x) => { x.insert(value); }
+            }
+        }
+    }
+
+    use std::fmt;
+
+    impl<T: PartialOrd + fmt::Display> fmt::Display for BTree<T> {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match self.root {
+                None => write!(f, "()"),
+                Some(ref x) => write!(f, "{}", x.construct_disp("")),
             }
         }
     }
@@ -37,20 +51,58 @@ pub mod tree {
         right_child: Option<Box<BNode<T>>>,
     }
 
-    impl<T: PartialOrd> BNode<T>{
-        fn insert(&mut self, value: T){
+    impl<T: PartialOrd> BNode<T> {
+        fn insert(&mut self, value: T) {
             if self.value == value {
                 return;
             }
-            let current_node = if value < self.value {&mut self.left_child} else {&mut self.right_child};
+            let current_node = if value < self.value { &mut self.left_child } else { &mut self.right_child };
             match current_node {
                 &mut Some(ref mut subnode) => subnode.insert(value),
                 &mut None => {
-                    let new_node = BNode {value, left_child: None, right_child: None};
+                    let new_node = BNode { value, left_child: None, right_child: None };
                     let boxed_node = Some(Box::new(new_node));
                     *current_node = boxed_node;
                 }
             }
+        }
+    }
+
+
+    //  ├ │ ─ └
+
+    impl<T: PartialOrd + fmt::Display> BNode<T> {
+        fn construct_disp(&self, prefix: &str) -> String {
+            let mut holder = String::new();
+            holder.push_str(&prefix);
+            holder.push('─');
+            holder.push_str(&self.value.to_string());
+            holder.push('\n');
+
+            match (&self.left_child, &self.right_child) {
+                (None, None) => (),
+                (Some(l), None) => {
+                    let mut new_prefix = String::from(prefix);
+                    new_prefix.push_str(" └");
+                    holder.push_str(&l.construct_disp(&new_prefix));
+                },
+                (None, Some(r)) => {
+                    let mut new_prefix = String::from(prefix);
+                    new_prefix.push_str(" └");
+                    holder.push_str(&r.construct_disp(&new_prefix));
+                },
+                (Some(l), Some(r)) => {
+                    let mut new_prefix = String::new();
+                    new_prefix.push_str(&prefix);
+                    new_prefix.push_str(" ├");
+                    holder.push_str(&l.construct_disp(&new_prefix));
+                    new_prefix = String::new();
+                    new_prefix.push_str(&prefix);
+                    new_prefix.push_str(" └");
+                    holder.push_str(&r.construct_disp(&new_prefix));
+                },
+            }
+            holder
         }
     }
 }
@@ -70,6 +122,6 @@ mod tests {
         b.insert(4);
         b.insert(6);
         b.insert(8);
-        println!("{:?}", b);
+        println!("{}", b);
     }
 }
