47c1475667abd272abe2e0ddb673270e632cc2b6
teiserse <teiserse@tcd.ie>
2018-10-18 23:49:26 +0100

    Re-implemented find function

--- src/dag.rs
+++ src/dag.rs
@@ -9,21 +9,28 @@ impl<T: Eq> DAG<T> {
         let mut new_dag = DAG {
             structure: Vec::new()
         };
-        new_dag.structure.push(DAGNode{
+        new_dag.structure[0] = DAGNode{
             value,
             children: Vec::new(),
             parents: Vec::new(),
-        });
+        };
         new_dag
     }
 
-    pub fn find<>(&self, value: &T) -> Option<&T> {
-        None
+    pub fn find(&self, value: &T) -> Option<&T> {
+        if self.structure[0].value == *value {
+            Some(&self.structure[0].value)
+        } else {
+            match self.structure[0].find(self,value) {
+                None => None,
+                Some(node) => Some(&node.value)
+            }
+        }
     }
 
-    pub fn add_new(&mut self, _parent: &T, _value: T) {
+    //pub fn add_new(&mut self, _parent: &T, _value: T) {
 
-    }
+    //}
 }
 
 #[derive(Debug)]
@@ -34,13 +41,26 @@ struct DAGNode<T: Eq> {
 }
 
 impl<T: Eq> DAGNode<T> {
-    fn find(&self, value: &T) -> Option<&DAGNode<T>> {
-        None
+    fn find<'a>(&'a self, holder :&'a DAG<T>, value: &T) -> Option<&'a DAGNode<T>> {
+        if self.value == *value {
+            Some(&self)
+        } else {
+            let mut ret = None;
+            for i_child in &self.children {
+                match ret {
+                    None => {
+                        ret = holder.structure[*i_child].find(holder,value);
+                    },
+                    Some(_) => break,
+                }
+            }
+            ret
+        }
     }
 
-    fn add_new(&mut self, parent: &T, value: T) {
+    //fn add_new(&mut self, parent: &T, value: T) {
 
-    }
+    //}
 }
 
 #[cfg(test)]
